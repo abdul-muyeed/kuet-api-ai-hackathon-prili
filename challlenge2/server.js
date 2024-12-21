2f83177f91e39299c4aa1c8a5a66b44eb820296e
Abdul Muyeed <abdulmuyeed47@gmail.com>
2024-12-21 20:42:44 +0600

    ingrdiants crud added

--- challlenge2/server.js
+++ challlenge2/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import Ingredients from "./models/Ingredients.js";
 
 dotenv.config();
 const app = express();
@@ -13,32 +14,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
-
 const PORT = process.env.PORT || 3000;
 
+app.get("/ingredients", (req, res) => {
+  const ingredients = Ingredients.find();
+  res.status(200).send(ingredients);
+});
 
-app.get('/ingredients', (req, res) => {
-    res.json(ingredients);
-  });
+app.post("/ingredients", (req, res) => {
+  const ingredient = req.body;
+  Ingredients.create(ingredient);
+  res.status(201).send(ingredient);
+});
 
+app.put("/ingredients/:id", (req, res) => {
+  const { id } = req.params;
+  const ingredient = req.body;
+  Ingredients.findByIdAndUpdate(id, ingredient, { new: true });
+  res.status(200).send(ingredient);
+});
 
+app.delete("/ingredients/:id", (req, res) => {
+  const { id } = req.params;
+  Ingredients.findByIdAndRemove(id);
+  res.status(200).send("Ingredient deleted successfully");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mongoose.connect(process.env.MONGOOSE_URI).then(() =>{
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+mongoose
+  .connect(process.env.MONGOOSE_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
-  .catch((error) => console.log('error',error.message));
+  .catch((error) => console.log("error", error.message));
