53f0bf4da412876ba9d3b5fa7bbdb7e34f6deb10
Abdul Muyeed <abdulmuyeed47@gmail.com>
2024-12-21 20:32:15 +0600

    epress app runinng

---
+++ challlenge2/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+mongoose.connect(process.env.MONGOOSE_URI).then(() =>{
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  })
+  .catch((error) => console.log('error',error.message));
